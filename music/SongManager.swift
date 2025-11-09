2c30405a31d03d34d9c7673067bacff8f367274f
Nikolai <nezzabudka5@gmail.com>
2025-11-09 02:10:44 +0100

    play music

--- music/SongManager.swift
+++ music/SongManager.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 class SongManager: ObservableObject {
     @Published private(set) var song: SongsModel = SongsModel(artist: "", audio_url: "", cover: "", title: "")
     
+    private var player: AVPlayer?
+    
     func playSong(song: SongsModel) {
         self.song = song
+        if let url = URL(string: song.audio_url), !song.audio_url.isEmpty {
+            player = AVPlayer(url: url)
+            player?.play()
+        } else {
+            player = nil
+        }
     }
     
+    func pause() {
+        player?.pause()
+    }
 }
